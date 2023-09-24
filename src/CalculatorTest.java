45ec6bd510b3479cc47d8fb4127597012c6368ab
unknown <smtishkar@mail.ru>
2023-09-24 14:09:33 +0300

    добавил метод расчета цены после скидки и его unit test

---
+++ src/CalculatorTest.java
@@ -0,0 +1,41 @@
+import seminars.first.model.Calculator;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        // Проверка базового функционала с целыми числами:
+        if (8 != Calculator.calculation(2, 6, '+')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (0 != Calculator.calculation(2, 2, '-')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (14 != Calculator.calculation(2, 7, '*')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (2 != Calculator.calculation(100, 50, '/')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        // Случаи с неправильными аргументами
+        // аргумент operator типа char, должен вызывать исключение, если он получает не базовые символы (+-*/)
+        try {
+            Calculator.calculation(8, 4, '_');
+        } catch (IllegalStateException e) {
+            if (!e.getMessage().equals("Unexpected value operator: _")) {
+                throw new AssertionError("Ошибка в методе");
+            }
+        }
+
+        //Проверка правильности подсчета суммы после скидки
+        assertThat(Calculator.calculatingDiscount(100,10)).isEqualTo((90));
+//        assertThat(Calculator.calculatingDiscount(100,-10)).isEqualTo((90));
+
+
+    }
+
+
+}
